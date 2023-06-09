d5ee3f9c4d02b65c3aa6eef3f888240d3daecd3c
Loominary <rustiezbow@gmail.com>
2023-06-09 14:03:21 +0300

    quill to output

--- project-1/src/app/app.component.ts
+++ project-1/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Delta } from 'quill';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
+  editorForm!: FormGroup;
+  editorContent!:string;
+  editorStyle ={
+    height: '400px;',
+    width: '300px;'
+  }
+  //formData = this.editorForm.get('editor')?.value;
+constructor(){
+  //this.editorForm.value= 'new FormGroup';
+}
+
+  ngOnInit(): void {   
+    this.editorForm = new FormGroup({
+    'editor': new FormControl(null)
+  })
+}
+
+
+
+  onSubmit(){
+    /*  if (this.editorForm.get('editor')!.value == undefined){
+      console.log("Can't send empty");
+      
+     } else{
+      console.log('text');
+
+     } */
+     console.log(this.editorForm.get('editor')?.value);
+     this.editorContent = this.editorForm.get('editor')?.value;
+     
+
+    
+  }
   title = 'project-1';
 }
