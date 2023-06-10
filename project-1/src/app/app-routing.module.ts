23964b26ff7abfce10083822bff931dd5b72d724
Loominary <rustiezbow@gmail.com>
2023-06-10 08:25:40 +0300

    template component

--- project-1/src/app/app-routing.module.ts
+++ project-1/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemplatesComponent } from './templates/templates.component';
+import { QuillEditorComponent } from './quill-editor/quill-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'templates', component:TemplatesComponent},
+  {path:'quill-editor', component:QuillEditorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
